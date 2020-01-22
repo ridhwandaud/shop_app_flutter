5fcfaa1d75d6d7bb297c1bdc92d3eff6f9e855f9
ridhwan <wan@pahanggo.com>
2020-01-22 16:03:21 +0800

    Initial commit and add custom fonts

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import './screens/product_overview_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shop App',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange
+      ),
+      home: ProductOverviewScreen()
+    );
+  }
+}
