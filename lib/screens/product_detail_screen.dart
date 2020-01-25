6eab7e18055329b961e991229d61c11a81750ee9
Ridhwan <muhammadridhwandaud@gmail.com>
2020-01-26 00:27:31 +0800

    Add listening to product detail screen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   
@@ -7,10 +9,10 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String; // is the id!
-    // ...
+    final loadedProduct = Provider.of<Products>(context).findById(productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text(productId),
+        title: Text(loadedProduct.title),
       ),
       body: Center(child: Text('Center'),),
     );
