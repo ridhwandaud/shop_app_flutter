8a0a598e8c831ebeb8ba1bfff608ba2dd2b447ff
ridhwandaud <m.ridhwan.daud@gmail.com>
2020-02-13 08:49:26 +0800

    Split orderbutton to own widget

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -26,16 +26,7 @@ class CartScreen extends StatelessWidget {
                 Text('Total', style: TextStyle(fontSize: 20),),
                 Spacer(),
                 Chip(label: Text('\$${cart.totalAmount.toString()}', style: TextStyle(color: Colors.white),), backgroundColor: Theme.of(context).primaryColor,),
-                FlatButton(
-                  child: Text('Order Now'), 
-                  onPressed: (){
-                    Provider.of<Orders>(context, listen: false).addOrder(cart.items.values.toList(), cart.totalAmount);
-                    cart.clear();
-                    // Navigator.of(context).pushNamed(
-                    //   OrdersScreen.routeName
-                    // );
-                  }, 
-                  textColor: Theme.of(context).primaryColor,)
+                OrderButton(cart: cart)
               ],
             ),
           ),
@@ -58,5 +49,42 @@ class CartScreen extends StatelessWidget {
         )
       ],),
     );
+  }
+}
+
+class OrderButton extends StatefulWidget {
+  const OrderButton({
+    Key key,
+    @required this.cart,
+  }) : super(key: key);
+
+  final Cart cart;
+
+  @override
+  _OrderButtonState createState() => _OrderButtonState();
+}
+
+class _OrderButtonState extends State<OrderButton> {
+
+  var _isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      child: _isLoading ? CircularProgressIndicator() : Text('Order Now'), 
+      onPressed: (widget.cart.totalAmount <= 0 || _isLoading) ? null : () async{
+        setState(() {
+          _isLoading = true;
+        });
+        await Provider.of<Orders>(context, listen: false).addOrder(widget.cart.items.values.toList(), widget.cart.totalAmount);
+        setState(() {
+          _isLoading = false;
+        });
+        widget.cart.clear();
+        // Navigator.of(context).pushNamed(
+        //   OrdersScreen.routeName
+        // );
+      }, 
+      textColor: Theme.of(context).primaryColor,);
   }
 }
