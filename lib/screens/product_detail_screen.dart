406edcaaf20b9ff2889a08eb2c3a1b1427d4b509
ridhwan <wan@pahanggo.com>
2020-03-12 16:36:48 +0800

    Add slilver

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -11,16 +11,14 @@ class ProductDetailScreen extends StatelessWidget {
     final productId = ModalRoute.of(context).settings.arguments as String; // is the id!
     final loadedProduct = Provider.of<Products>(context).findById(productId);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(loadedProduct.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 300,
-              width: double.infinity,
-              child: Hero(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(loadedProduct.title),
+              background: Hero(
                 tag: loadedProduct.id,
                   child: Image.network(
                   loadedProduct.imageUrl,
@@ -28,6 +26,8 @@ class ProductDetailScreen extends StatelessWidget {
                 ),
               ),
             ),
+          ),
+          SliverList(delegate: SliverChildListDelegate([
             SizedBox(height: 10,),
             Text('\$${loadedProduct.price}', style: TextStyle(color: Colors.grey, fontSize: 20),),
             SizedBox(height: 10,),
@@ -35,9 +35,10 @@ class ProductDetailScreen extends StatelessWidget {
               padding: EdgeInsets.symmetric(horizontal: 10),
               width: double.infinity,
               child: Text(loadedProduct.description, textAlign: TextAlign.center, softWrap: true,)
-            )
-          ],
-        ),
+            ),
+            SizedBox(height: 800,)
+          ])
+        )],
       ),
     );
   }
